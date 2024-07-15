5d9a7dd91e6d81098b5fa6ada921722938e49f0a
It-s-my <vedeneevvd@gmail.com>
2024-07-15 17:46:32 +0300

    Поправил структуру в datats

--- static/script/Data.ts
+++ static/script/Data.ts
@@ -5,7 +5,7 @@ import {ElemCreate} from "./ElementCreate";
 import {navigateToDirectory} from "./Navigate";
 
 //upload - загружает все файлы на стрнице по текущему пути
-export async function upload(currentPath: string, sortFlag: boolean) {
+export function upload(currentPath: string, sortFlag: boolean) {
 
     Spin.on();
     let sort = sortAsc;
@@ -15,23 +15,26 @@ export async function upload(currentPath: string, sortFlag: boolean) {
     }
 
     const url = `/fs?root=${currentPath}&sort=${sort}`;
-    console.log(url)
-    console.log(currentPath)
-    const response = await fetch(url, {
+    console.log(url);
+    console.log(currentPath);
+
+    fetch(url, {
         method: "GET",
     })
         .then(response => response.json())
         .then(data => {
-            console.log(currentPath)
-            if(data["Status"]===200){
-                console.log(currentPath)
-                ElemCreate(data)
-            }else{
-                alert(data["Error"])
-            }})
+            console.log(currentPath);
+            if (data["Status"] === 200) {
+                console.log(currentPath);
+                ElemCreate(data);
+            } else {
+                alert(data["Error"]);
+            }
+        })
         .catch(error => {
             console.error('Ошибка:', error);
+        })
+        .finally(() => {
+            Spin.off();
         });
-
-    Spin.off();
 }
